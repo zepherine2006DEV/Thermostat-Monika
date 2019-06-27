8f52ec50797ddf95be004cb2a3ac5d89a68b3800
Student <kaczanowskamonika@gmail.com>
2019-06-27 14:05:49 +0100

    power switch is off and on requirements fullfilled

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -32,16 +32,27 @@ describe("Thermostat", function() {
      }
 
     expect(thermostat.down()).toEqual(10);
-  })
+  });
 
   it("has a maximum temperature of 25 degrees if power saving mode is on", function() {
 
-    thermostat.powerSavingOn()
+    thermostat.powerSavingOn();
 
     var i;
-    for(i=20; i <= 25; i++) {
+    for(i=20; i < 24; i++) {
       thermostat.up()
     }
-    expect(thermostat.up()).toEqual(25)
-  })
+    expect(thermostat.up()).toEqual(25);
+  });
+
+  it("has a maximum temperature of 32 degrees if power saving mode is off", function() {
+    thermostat.powerSavingOff();
+
+    var i;
+    for(i=20; i < 31; i++) {
+      thermostat.up()
+    }
+    expect(thermostat.up()).toEqual(32);
+
+  });
 });
--- src/thermostat.js
+++ src/thermostat.js
@@ -1,22 +1,25 @@
 function Thermostat() {
-
+  this.savingSwitch = true;
   const DEFAULTTEMP = 20;
   const MINTEMP = 10;
   const MAXTEMPONPOWERSAVING= 25;
+  const MAXTEMPONPOWERSAVINGOFF = 32;
 
   this.defaultTemperature = function() {
       return DEFAULTTEMP;
   };
 
-  this.temp = DEFAULTTEMP
+  this.temp = DEFAULTTEMP;
 
   this.up = function(){
   // if temp > 25  and power switch is on then we can't have it higher
-    if (this.temp < MAXTEMPONPOWERSAVING) {
-
-      this.temp ++;
-    }
-    return this.temp
+    if (this.temp < MAXTEMPONPOWERSAVING && this.savingSwitch===true) {
+      this.temp++;
+    } else if (this.temp < MAXTEMPONPOWERSAVINGOFF && this.savingSwitch===false) {
+        this.temp ++;
+      }
+    
+    return this.temp;
   };
 
   this.down = function() {
@@ -25,15 +28,21 @@ function Thermostat() {
       this.temp --;
     }
       return this.temp;
-  }
+  };
 
   this.minTemp = function() {
     return MINTEMP;
-  }
+  };
 
   this.powerSavingOn = function(){
-    this.switch = true
-  }
+    this.savingSwitch = true
+  };
+
+  this.powerSavingOff = function(){
+    this.savingSwitch = false
+  };
+
+
 };
 
 //Thermostat.prototype.defaultTemperature = function() {
